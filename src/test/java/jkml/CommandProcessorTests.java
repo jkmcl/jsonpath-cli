7ceefce1aa8176bd43a20d456540bfb72d4cb9b6
Michael Lee <jkmcl@users.noreply.github.com>
2026-07-24 00:51:52 +0800

    Dependency upgrade

--- src/test/java/jkml/CommandProcessorTests.java
+++ src/test/java/jkml/CommandProcessorTests.java
@@ -10,25 +10,25 @@ class CommandProcessorTests {
 	@Test
 	void testNoArg() {
 		var cp = new CommandProcessor();
-		assertNotEquals(0, cp.run(new String[0]));
+		assertNotEquals(0, cp.run());
 	}
 
 	@Test
 	void testString() {
 		var cp = new CommandProcessor();
-		assertEquals(0, cp.run(new String[] { "-p", "$", "-s", "{}", "--pretty" }));
+		assertEquals(0, cp.run("-p", "$", "-s", "{}", "--pretty"));
 	}
 
 	@Test
 	void testFile() {
 		var cp = new CommandProcessor();
-		assertEquals(0, cp.run(new String[] { "-p", "$", "-f", "src/test/resources/example.json" }));
+		assertEquals(0, cp.run("-p", "$", "-f", "src/test/resources/example.json"));
 	}
 
 	@Test
 	void testException() {
 		var cp = new CommandProcessor();
-		assertNotEquals(0, cp.run(new String[] { "-p", "$", "-f", "src/test/resources/noSuchFile.json" }));
+		assertNotEquals(0, cp.run("-p", "$", "-f", "src/test/resources/noSuchFile.json"));
 	}
 
 }
